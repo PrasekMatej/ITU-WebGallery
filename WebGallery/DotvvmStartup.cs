dddfbc9f992433ecd3b1176a68d67a30ec8b19ed
Michal Tichý <michal.tichy@riganti.cz>
2019-10-16 09:30:57 +0200

    Added bootstrap library

--- WebGallery/DotvvmStartup.cs
+++ WebGallery/DotvvmStartup.cs
@@ -1,4 +1,5 @@
 ﻿using DotVVM.Framework.Configuration;
+using DotVVM.Framework.Controls.Bootstrap4;
 using DotVVM.Framework.ResourceManagement;
 using DotVVM.Framework.Routing;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,7 @@ namespace WebGallery
         // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
         public void Configure(DotvvmConfiguration config, string applicationPath)
         {
+            config.AddBootstrap4Configuration();
             ConfigureRoutes(config, applicationPath);
             ConfigureControls(config, applicationPath);
             ConfigureResources(config, applicationPath);
@@ -18,7 +20,7 @@ namespace WebGallery
         private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
         {
             config.RouteTable.Add("Default", "", "Views/Default.dothtml");
-            config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));    
+            config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));
         }
 
         private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
@@ -29,15 +31,15 @@ namespace WebGallery
         private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
         {
             // register custom resources and adjust paths to the built-in resources
-			config.Resources.Register("Styles", new StylesheetResource()
+            config.Resources.Register("Styles", new StylesheetResource()
             {
                 Location = new UrlResourceLocation("~/styles.css")
             });
         }
 
-		public void ConfigureServices(IDotvvmServiceCollection options)
+        public void ConfigureServices(IDotvvmServiceCollection options)
         {
             options.AddDefaultTempStorages("temp");
-		}
+        }
     }
 }
